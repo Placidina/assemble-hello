44e923d7017221e35d7aa837c7cab03e5f16b3eb
Placidina <alanplacidina@gmail.com>
2019-02-08 21:37:12 -0200

    identation: fix

--- src/main.asm
+++ src/main.asm
@@ -11,19 +11,20 @@ default rel
 global _start
 
 
-section	.text
+section .text
+
 _start:                 ;tell linker entry point
-    mov	edx,len         ;message length
-    mov	esi,msg         ;message to write
-    mov	edi,stdout      ;file descriptor (stdout)
-    mov	eax,sys_write   ;system call number (sys_write)
+    mov edx,len         ;message length
+    mov esi,msg         ;message to write
+    mov edi,stdout      ;file descriptor (stdout)
+    mov eax,sys_write   ;system call number (sys_write)
     syscall             ;call kernel
     xor edi,edi         ;clean to exit code 0
-    mov	eax,sys_exit    ;system call number (sys_exit)
+    mov eax,sys_exit    ;system call number (sys_exit)
     syscall             ;call kernel
 
 
-section	.rodata
+section .rodata
 
 msg: db 'Hello, world!', 0  ;our dear string
 len: equ $ - msg            ;length of our dear string
