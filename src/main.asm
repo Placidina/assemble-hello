eec8b91b92f7b38de01a3af444e793b42f3bff08
Placidina <alanplacidina@gmail.com>
2019-02-08 22:34:02 -0200

    comments

--- src/main.asm
+++ src/main.asm
@@ -1,30 +1,22 @@
-;Assembly Hello World
-
-sys_exit    equ 60
-sys_write   equ 1
-
-stdin   equ 0
-stdout  equ 1
-stderr  equ 2
-
-default rel
 global _start
 
+section .data
+    align 2
+    msg: db 'Hello World!',0x0a
+    len: equ $-msg
+
+section .bss
 
 section .text
+    _start:
+        ; ssize_t write(int fd, const void *buf, size_t count)
+        mov edx,len ; Set length of the message
+        mov esi,msg ; Set address of the message
+        mov edi,1   ; Set file descriptor number: 1 (stdout)
+        mov eax,1   ; sys_write system number: 1
+        syscall     ; Call it
 
-_start:                 ;tell linker entry point
-    mov edx,len         ;message length
-    mov esi,msg         ;message to write
-    mov edi,stdout      ;file descriptor (stdout)
-    mov eax,sys_write   ;system call number (sys_write)
-    syscall             ;call kernel
-    xor edi,edi         ;clean to exit code 0
-    mov eax,sys_exit    ;system call number (sys_exit)
-    syscall             ;call kernel
-
-
-section .rodata
-
-msg: db 'Hello, world!', 0  ;our dear string
-len: equ $ - msg            ;length of our dear string
+        ; void exit(int status)
+        xor edi,edi ; Set exit status to 0
+        mov eax,60  ; sys_exit system number: 60
+        syscall     ; Call it
